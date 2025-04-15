d822d9746b1541f6c2c268ced3503298554bd251
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-04-15 11:23:51 -0300

    ajuste no layout index e show de ambiente virtual

--- resources/views/ambiente-virtual/index.blade.php
+++ resources/views/ambiente-virtual/index.blade.php
@@ -52,30 +52,31 @@
         <a class="btn btn-success" href="{{route('ambiente-virtual.create')}}">Adicionar nova aula</a>
     </div>
     @endif
-    <div class="row">
-        <div class="col mt-4">
+    <div class="container-xl">
+    <div class="row row-deck row-cards">
+        <div class="col-12">
             @if($aulas->isEmpty())
             <p>Nenhum registro encontrado.</p>
             @else
                     @foreach($aulas as $aula)
-                            <div class="col-md-6">
+                            <div class="col-6 p-2">
                 <div class="card">
                     <div class="card-header">
                         <div class="col-6">
                             <h3 class="card-title">{{ $aula->titulo }}</h3>
                             <p class="card-subtitle">{{ $aula->professor->NomeProfessor }} - 12/03/2025</p>
                         </div>
-                        <div class="col-6 card-actions">
-                            <a href="{{route('ambiente-virtual.show', $aula)}}" class="btn btn-primary btn-2 col-3 p-2">Assistir</a>
+                        <div class="col-6 card-actions ">
+                            <p><a href="{{route('ambiente-virtual.show', $aula)}}" class="btn btn-primary btn-2 col-4">Assistir</a></p>
                                 @if($user->role != 'aluno')
-                            <a class="btn btn2 col-3 p-2" href="{{route('ambiente-virtual.edit', $aula)}}">
+                            <a class="btn btn2 col-4" href="{{route('ambiente-virtual.edit', $aula)}}">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
                                 Editar
                             </a>
                             <form action="{{route('ambiente-virtual.destroy', $aula->id)}}" method="POST" class="d-inline">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger ms-8 col-3 pt-2">
+                                <button type="submit" class="btn btn-danger ms-8 col-4">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>    
                                 
                                 Excluir</button>
@@ -91,6 +92,7 @@
 
                     @endforeach
             @endif
+        </div>
         </div>
     </div>
     <div class="row">
--- resources/views/ambiente-virtual/show.blade.php
+++ resources/views/ambiente-virtual/show.blade.php
@@ -7,106 +7,108 @@
 <div class="container">
     <!-- PAGE HEADER -->
     <div class="row">
-        <div class="col-12 text-center">
-            <h1>Aula: {{ $aula->titulo }} (Professor: {{ $aula->professor->NomeProfessor }})</h1>
-            <h3 class="text-muted">@php echo strip_tags($aula->descricao); @endphp</h3>
-            <h3 class="text-muted">Disciplina: {{ $aula->disciplina->nome }}</h3>
-        </div>
-    </div>
-    @if(Session::has('success'))
-    <div class="row mt-2">
-        <div class="col-6 m-auto">
-            <div class="alert alert-success text-center" role="alert">
-                {!! session::get('success') !!}
-            </div>
-        </div>
-    </div>
-    @endif
-    @if(Session::has('error'))
-    <div class="row mt-2">
-        <div class="col-6 m-auto">
-            <div class="alert alert-danger text-center" role="alert">
-                {!! session::get('error') !!}
-            </div>
-        </div>
-    </div>
-    @endif
-
-    <div class="row">
-        <div class="col mt-4 mb-4">
-            <div class="plyr__video-embed" id="player" data-plyr-provider="youtube" data-plyr-embed-id="{{ $aula->yt_url }}" >
-                <video>
-                    <source src="{{ $aula->yt_url }}" type="video/mp4">
-                </video>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col mt-4">
-            <h1>Anotações</h1><?php //dd($aula->nota) ?>
-            @foreach($aula->nota as $nota)
-            @if(Auth::user()->id === $nota->user_id)
-            <div class="card mb-2">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-10">
-                            <p>Nota: <?php echo strip_tags($nota->nota); ?></p>
-                        </div>
+        <div class="col-8">
+            @if(Session::has('success'))
+            <div class="row mt-2">
+                <div class="col-6 m-auto">
+                    <div class="alert alert-success text-center" role="alert">
+                        {!! session::get('success') !!}
                     </div>
                 </div>
             </div>
             @endif
-            @endforeach
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-body">
-            <form action="{{ route('ambiente-virtual.anotar', $aula) }}" method="POST">
-                @csrf
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}" />
-                <input type="hidden" name="ambiente_virtual_id" value="{{ $aula->id }}" />
-                <div class="form-group">
-                    <label class="mb-2" for="comentarios">Anotar</label>
-                    <textarea class="form-control" id="nota" name="nota" rows="3"></textarea>
+            @if(Session::has('error'))
+            <div class="row mt-2">
+                <div class="col-6 m-auto">
+                    <div class="alert alert-danger text-center" role="alert">
+                        {!! session::get('error') !!}
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary mt-2">Anotar</button>
-            </form>
-        </div>
-    </div>
+            </div>
+            @endif
 
-    <div class="row">
-        <div class="col mt-4">
-            <h1>Comentarios</h1>
-            @foreach($aula->comentario as $comentario)
-            <div class="card mb-2">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-8">
-                            <p>Comentario: <?php echo strip_tags($comentario->comentario); ?></p>
+            <div class="row">
+                <div class="col mt-4 mb-4">
+                    <div class="plyr__video-embed" id="player" data-plyr-provider="youtube" data-plyr-embed-id="{{ $aula->yt_url }}" >
+                        <video>
+                            <source src="{{ $aula->yt_url }}" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col mt-4">
+                    <h2>Comentarios</h2>
+                    @foreach($aula->comentario as $comentario)
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-8">
+                                    <p>Comentario: <?php echo strip_tags($comentario->comentario); ?></p>
+                                </div>
+                                <div class="col">
+                                    <p>Aluno: {{ $comentario->user->aluno->NomeAluno ?? $comentario->user->name }}</p>
+                                </div>
+                                <div class="col">
+                                    <p>Data: {{ $comentario->created_at->format('d/m/Y') }}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col">
-                            <p>Aluno: {{ $comentario->user->aluno->NomeAluno ?? $comentario->user->name }}</p>
-                        </div>
-                        <div class="col">
-                            <p>Data: {{ $comentario->created_at->format('d/m/Y') }}</p>
+                    </div>
+                    @endforeach
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('ambiente-virtual.comentar', $aula) }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}" />
+                                <input type="hidden" name="ambiente_virtual_id" value="{{ $aula->id }}" />
+                                <div class="form-group">
+                                    <label class="mb-2" for="comentarios">Comentar</label>
+                                    <textarea class="form-control" id="comentarios" name="comentario" rows="3"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary mt-2">Comentar</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
+        </div>
+
+        <div class="col-4">
+            <h1 class="mt-4">Aula: {{ $aula->titulo }} (Professor: {{ $aula->professor->NomeProfessor }})</h1>
+                <p>@php echo strip_tags($aula->descricao); @endphp</p>
+                <div><strong>Disciplina:</strong> {{ $aula->disciplina->nome }}</div>
+
+                <div class="row">
+            <div class="col mt-4">
+                <?php //dd($aula->nota) ?>
+                @foreach($aula->nota as $nota)
+                @if(Auth::user()->id === $nota->user_id)
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-10">
+                                <p>Nota: <?php echo strip_tags($nota->nota); ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                @endforeach
+            </div>
+
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ route('ambiente-virtual.comentar', $aula) }}" method="POST">
+                    <form action="{{ route('ambiente-virtual.anotar', $aula) }}" method="POST">
                         @csrf
                         <input type="hidden" name="user_id" value="{{ Auth::user()->id }}" />
                         <input type="hidden" name="ambiente_virtual_id" value="{{ $aula->id }}" />
                         <div class="form-group">
-                            <label class="mb-2" for="comentarios">Comentar</label>
-                            <textarea class="form-control" id="comentarios" name="comentario" rows="3"></textarea>
+                            <label class="mb-2" for="comentarios">Anotações</label>
+                            <textarea class="form-control" id="nota" name="nota" rows="3"></textarea>
                         </div>
-                        <button type="submit" class="btn btn-primary mt-2">Comentar</button>
+                        <button type="submit" class="btn btn-primary mt-2">Anotar</button>
                     </form>
                 </div>
             </div>
@@ -125,12 +127,14 @@
             selector: "#comentarios",
             language: "pt_BR",
             menubar: false,
+            height: 200,
             license_key: 'gpl',
         }
 
         let optionsNotas = {
             selector: "#nota",
             language: "pt_BR",
+            height: 150,
             menubar: false,
             license_key: 'gpl',
         }
