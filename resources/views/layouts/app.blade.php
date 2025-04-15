db96e6773e78c4c6c05e5ee2252abfa3e11cdb2b
bobmw <bobmwendell@gmail.com>
2025-04-15 15:34:55 -0300

    fix: #166 conteudo principal dentro da div page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
             }
         @endphp
     @endauth
-    <div class="page">
+    <div>
         <!-- Navbar -->
         @guest
             <header class="navbar navbar-expand-md d-print-none">
@@ -738,7 +738,47 @@
                                         </li>
                                     @endif
                                 @endif
+                                @if ($ambiente_virtual)
+                                    <li
+                                        class="nav-item {{ request()->routeIs('ambiente-virtual.index') ? 'bg-primary text-white rounded' : '' }}">
+                                        <a class="nav-link" href="{{ route('ambiente-virtual.index') }}">
+                                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-checklist">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path
+                                                        d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" />
+                                                    <path d="M14 19l2 2l4 -4" />
+                                                    <path d="M9 8h4" />
+                                                    <path d="M9 12h2" />
+                                                </svg>
+                                            </span>
+                                            {{ __('Ambiente virtual') }}
+                                        </a>
+                                    </li>
+                                @endif
 
+                                @if (Session::get('role') === 'administrador')
+                                    <li
+                                        class="nav-item {{ request()->routeIs('disciplinas.index') ? 'bg-primary text-white rounded' : '' }} ">
+                                        <a class="nav-link" href="{{ route('disciplinas.index') }}">
+                                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-settings">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path
+                                                        d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                                                    <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                                </svg>
+                                            </span>
+                                            {{ __('Configurações') }}
+                                        </a>
+                                    </li>
+                                @endif
                                 <li
                                     class="nav-item {{ request()->routeIs('nucleo.material') ? 'bg-primary text-white rounded' : '' }}">
                                     <a class="nav-link" href="{{ route('nucleo.material') }}">
@@ -779,6 +819,7 @@
                     </div>
                 </div>
             </aside>
+
             <div class="page">
                 <header class="navbar-expand-md">
                     <div class="collapse navbar-collapse" id="navbar-menu">
@@ -1167,13 +1208,13 @@
                     </div>
 
                 </header>
+                @endauth
+                
+                <main class="py-4">
+                    @yield('content')
+                </main>
             </div>
-        @endauth
 
-
-        <main class="py-4">
-            @yield('content')
-        </main>
 
         @yield('js')
     </div>
