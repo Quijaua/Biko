a014550ecfbcb02846b68d93db993449d38495ec
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-04-15 09:58:35 -0300

    ajuste no layout index de ambiente virtual

--- resources/views/ambiente-virtual/index.blade.php
+++ resources/views/ambiente-virtual/index.blade.php
@@ -57,30 +57,49 @@
             @if($aulas->isEmpty())
             <p>Nenhum registro encontrado.</p>
             @else
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Título</th>
-                        <th scope="col">Capa</th>
-                        <th scope="col">Professor</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
                     @foreach($aulas as $aula)
-                    <tr>
-                        <td>{{ $aula->titulo }}</td>
-                        <td><img class="rounded-circle avatar" src="{{ asset('aulas-virtuais/imagens/' . $aula->id . '/' . $aula->imagem_capa) }}" alt="{{ $aula->titulo }}"></td>
-                        <td> {{ $aula->professor->NomeProfessor }}</td>
+                        <td></td>
+                        <td> </td>
                         <td>
-                            <a class="btn btn-primary" href="{{route('ambiente-virtual.show', $aula)}}">Assistir</a>
-                            @if($user->role != 'aluno')
-                            <a class="btn btn-warning" href="{{route('ambiente-virtual.edit', $aula)}}">Editar</a>
+
+
+
+                            <div class="col-md-6">
+                <div class="card">
+                  <div class="card-header">
+                    <div>
+                      <h3 class="card-title">{{ $aula->titulo }}</h3>
+                      <p class="card-subtitle">{{ $aula->professor->NomeProfessor }} - 12/03/2025</p>
+                    </div>
+                    <div class="card-actions">
+                      <a href="{{route('ambiente-virtual.show', $aula)}}" class="btn btn-primary btn-2">Assistir</a>
+
+                      @if($user->role != 'aluno')
+                      <a class="btn btn2" href="{{route('ambiente-virtual.edit', $aula)}}">
+                      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
+                        Editar
+                      </a>
+
+
                             <form action="{{route('ambiente-virtual.destroy', $aula->id)}}" method="POST" class="d-inline">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Excluir</button>
+                                <button type="submit" class="btn btn-danger ms-8">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>    
+                                
+                                Excluir</button>
                             </form>
+
+
+                    </div>
+                  </div>
+                  <div class="card-body p-0">
+                  <img class="drounded-circle davatar" src="{{ asset('aulas-virtuais/imagens/' . $aula->id . '/' . $aula->imagem_capa) }}" alt="{{ $aula->titulo }}">
+                  </div>
+                </div>
+              </div>
+
+
                             @endif
                         </td>
                     </tr>
