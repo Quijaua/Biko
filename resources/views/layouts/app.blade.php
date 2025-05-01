651d0946addbbcdaef6fc6b456ced57cf1452d6e
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-05-01 17:29:09 -0300

    adicionando link pro painel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -472,6 +472,25 @@
                                 </li>
                             @endif
 
+                            @if (Session::get('role') === 'coordenador')
+                                <li
+                                    class="nav-item {{ request()->routeIs('disciplinas.index') ? 'bg-primary text-white rounded' : '' }} ">
+                                    <a class="nav-link" href="{{ route('disciplinas.index') }}">
+                                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="icon icon-tabler icons-tabler-outline icon-tabler-settings">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                    d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                                                <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                            </svg>
+                                        </span>
+                                        {{ __('Configurações') }}
+                                    </a>
+                                </li>
+                            @endif
                             <li
                                 class="nav-item  {{ request()->routeIs('messages.index') ? 'bg-primary text-white rounded' : '' }}">
                                 <a class="nav-link" href="{{ route('messages.index') }}">
@@ -537,7 +556,15 @@
                                 @endif
                                 @if (Session::get('role') !== 'aluno')
                                     @if (Session::get('verified'))
-                                     
+                                    <li class="nav-item {{ request()->is('dashboard') ? 'bg-primary text-white rounded' : '' }}">
+                                <a class="nav-link" href="/dashboard">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chart-infographic"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M7 3v4h4" /><path d="M9 17l0 4" /><path d="M17 14l0 7" /><path d="M13 13l0 8" /><path d="M21 12l0 9" /></svg>
+                                    </span>
+                                    {{ __('Painel') }}
+                                </a>
+                            </li>
+
                                         <li
                                             class="nav-item {{ request()->is('alunos/*') ? 'bg-primary text-white rounded' : '' }}">
                                             <a class="nav-link" href="/alunos">
