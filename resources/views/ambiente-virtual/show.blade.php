1a456e86e7be033f51c0387db2dffad0675d977e
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-05-15 16:53:44 -0300

    [MELHORIA] Em ambiente virtual - Incluir botão "Voltar" na tela de assistir aula #212

--- resources/views/ambiente-virtual/show.blade.php
+++ resources/views/ambiente-virtual/show.blade.php
@@ -29,6 +29,22 @@
 
             <div class="row">
                 <div class="col mt-4 mb-4">
+
+    <div>
+            <p style="font-size: 24px;"><span><a href="/ambiente-virtual" class="text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" style="width: 30px; height: 30px;" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="icon icon-tabler icons-tabler-outline icon-tabler-circle-arrow-left">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18" />
+                            <path d="M8 12l4 4" />
+                            <path d="M8 12h8" />
+                            <path d="M12 8l-4 4" />
+                        </svg>
+                    </a></span> Todas aulas</p>
+        </div>
+
                     <div class="plyr__video-embed" id="player" data-plyr-provider="youtube" data-plyr-embed-id="{{ $aula->yt_url }}" >
                         <video>
                             <source src="{{ $aula->yt_url }}" type="video/mp4">
@@ -75,8 +91,8 @@
             </div>
         </div>
 
-        <div class="col-4">
-            <h1 class="mt-4">Aula: {{ $aula->titulo }} (Professor: {{ $aula->professor->NomeProfessor }})</h1>
+        <div class="col-4 mt-5">
+            <h1 class="mt-6">Aula: {{ $aula->titulo }} (Professor: {{ $aula->professor->NomeProfessor }})</h1>
                 <p>@php echo strip_tags($aula->descricao); @endphp</p>
                 <div><strong>Disciplina:</strong> {{ $aula->disciplina->nome }}</div>
 
