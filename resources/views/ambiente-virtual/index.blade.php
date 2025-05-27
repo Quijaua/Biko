777a02c26364560c57f7a1fe7a292a80c64ff761
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-05-26 21:19:41 -0300

    ajuste na paginação do ambiente virtual

--- resources/views/ambiente-virtual/index.blade.php
+++ resources/views/ambiente-virtual/index.blade.php
@@ -68,7 +68,6 @@
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger ms-8 col-4">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>    
-                                
                                 Excluir</button>
                             </form>
                                 @endif
@@ -83,12 +82,14 @@
                     @endforeach
             @endif
         </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-        {{ $aulas->links() }}
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col mt">
+				{{ $aulas->links() }}
+			</div>
+		</div>
+	</div>
+</div>
+
 </div>
 @endsection
