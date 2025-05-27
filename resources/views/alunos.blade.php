8bedafa99bc7140081289250497ea356f88477bb
motive-mobi <motive.mobi@gmail.com>
2025-05-27 16:41:32 -0300

    [fix] ajuste na exibição de botões de edição e ativação/inativação para usuários com perfil professor

--- resources/views/alunos.blade.php
+++ resources/views/alunos.blade.php
@@ -223,6 +223,7 @@
                           <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                         </svg></span> Ver Detalhes
                     </a>
+                    @if($user->role === 'administrador' || $user->role === 'coordenador')
                     <a href="/alunos/edit/{{ $aluno->id }}" class="btn btn-primary">
                       <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
                           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -231,6 +232,7 @@
                           <path d="M16 5l3 3" />
                         </svg></span> Editar
                     </a>
+
                     @if($aluno->Status === 0)
                     <a href="/alunos/enable/{{ $aluno->id }}">
                       <span class="status-btn status-ativo ms-8">
@@ -245,6 +247,7 @@
                         Inativar
                       </span>
                     </a>
+                    @endif
                     @endif
                   </div>
                 </td>
