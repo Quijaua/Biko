ee7ceb34a315590187e8d82d717fb72314900908
motive-mobi <motive.mobi@gmail.com>
2025-08-07 17:13:01 -0300

    [feat] coordenador somente ativa/inativa coordernadores que ele criou

--- resources/views/coordenadores/coordenadores.blade.php
+++ resources/views/coordenadores/coordenadores.blade.php
@@ -237,7 +237,7 @@
                                                                 </svg></span> Editar
                                                         </a>
                                                         @endif
-                                                        @if (Session::get('role') !== 'professor')
+                                                        @if (Session::get('role') !== 'professor' && $coordenador->id_user == Auth::user()->id)
                                                             @if ($coordenador->Status === 1)
                                                             <a onclick="modalShow('Inativar coordenador', 'Tem certeza que deseja inativar esse coordenador?', 'danger', e => window.location.href = '/coordenadores/disable/{{ $coordenador->id }}');">
                                                                     <span class="status-btn status-inativo ms-8">
