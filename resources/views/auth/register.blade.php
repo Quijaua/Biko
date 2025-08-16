317e2b6c83d9228c768391277e6266b05dcab99c
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-08-16 18:21:07 -0300

    [fix] alterando title de register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -521,11 +521,11 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="successModalLabel">Modal title</h1>
+                        <h1 class="modal-title fs-5" id="successModalLabel">👏 Cadastro efetuado com sucesso</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Parabens, seu cadastro foi realizado com sucesso. Em breve uma pessoa da coordenação entrará em contato.
+                        Parabéns, seu cadastro foi realizado com sucesso. Em breve uma pessoa da coordenação entrará em contato.
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
