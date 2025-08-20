eaa03578f5b5513a9ab7049b08eea9efd9179f97
Cauã <cauaserpa092@gmail.com>
2025-08-20 01:58:31 -0300

    [fix] corrigir overflow em user_id alterando para BIGINT

---
+++ database/migrations/2025_08_20_015529_alter_user_id_in_psicologos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (DB::getSchemaBuilder()->getColumnType('psicologos', 'user_id') !== 'bigint') {
+            Schema::table('psicologos', function (Blueprint $table) {
+                $table->bigInteger('user_id')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('psicologos', function (Blueprint $table) {
+            $table->integer('user_id')->change();
+        });
+    }
+};
