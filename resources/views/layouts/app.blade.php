24dc3aa58c10e0676940e6bb008e55aea2f69590
Cauã <cauaserpa092@gmail.com>
2025-09-09 22:40:24 -0300

    [feat] exibir "Curso Preparatório" em vez de "Núcleo Virtual" para alunos do Núcleo EAD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,8 +92,10 @@
             }
 
             if ($user->role === 'aluno') {
-                $aluno_nucleo = $user->aluno->nucleo;
-                $aluno_nucleo->permite_ambiente_virtual ? ($ambiente_virtual = true) : ($ambiente_virtual = false);
+                $aluno_nucleo_id = $user->aluno->id_nucleo ?? null;
+                $nucleoAmbienteVirtual = env('NUCLEO_AMBIENTE_VIRTUAL');
+
+                $ambiente_virtual = ($aluno_nucleo_id && $aluno_nucleo_id == $nucleoAmbienteVirtual);
             } elseif ($user->role !== 'aluno') {
                 $ambiente_virtual = true;
             }
@@ -1549,6 +1551,26 @@
                                             {{ __('Curso Preparatório') }}
                                         </a>
                                     </li>
+                                @else
+                                <li
+                                    class="nav-item">
+                                    <a class="nav-link" href="{{ route('ambiente-virtual.index') }}">
+                                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="icon icon-tabler icons-tabler-outline icon-tabler-checklist">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                    d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" />
+                                                <path d="M14 19l2 2l4 -4" />
+                                                <path d="M9 8h4" />
+                                                <path d="M9 12h2" />
+                                            </svg>
+                                        </span>
+                                        {{ __('Núcleo Virtual') }}
+                                    </a>
+                                </li>
                                 @endif
 
                                                 @endif
