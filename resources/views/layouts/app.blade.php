ab1055d92c9b81706a214869b23e734e25bc17f4
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2025-09-20 19:10:21 -0300

    [hotfix] adicionando link de apoio emocional no menu do aluno

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -721,6 +721,29 @@
                                         {{ __('Meus dados') }}
                                     </a>
                                 </li>
+				@if ($ambiente_virtual)
+				<li class="nav-item {{ request()->is('plantao-psicologico') ? 'bg-primary text-white rounded' : '' }}">
+                                            <a class="nav-link" href="/plantao-psicologico">
+                                                <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="icon icon-tabler icons-tabler-outline icon-tabler-brain">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                        <path d="M15.5 13a3.5 3.5 0 0 0 -3.5 3.5v1a3.5 3.5 0 0 0 7 0v-1.8" />
+                                                        <path d="M8.5 13a3.5 3.5 0 0 1 3.5 3.5v1a3.5 3.5 0 0 1 -7 0v-1.8" />
+                                                        <path d="M17.5 16a3.5 3.5 0 0 0 0 -7h-.5" />
+                                                        <path d="M19 9.3v-2.8a3.5 3.5 0 0 0 -7 0" />
+                                                        <path d="M6.5 16a3.5 3.5 0 0 1 0 -7h.5" />
+                                                        <path d="M5 9.3v-2.8a3.5 3.5 0 0 1 7 0v10" />
+                                                    </svg>
+                                                </span>
+                                                {{ __('Apoio Emocional') }}
+                                            </a>
+                                        </li>
+				@else
+				@endif
+
                                 @endif
                                 @if (Session::get('role') !== 'aluno')
                                     <li class="nav-item {{ request()->is('dashboard') ? 'bg-primary text-white rounded' : '' }}">
