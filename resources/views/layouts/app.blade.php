478a54e1c5e39f54c7e2c5cc338e74e88b92f4a6
Rafael Bantu <rafaelgomes@quijaua.com.br>
2026-05-25 14:47:20 -0300

    removendo arquivo errado e corrigindo menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -231,6 +231,27 @@
                                         {{ __('Meus dados') }}
                                     </a>
                                 </li>
+
+                                <li
+                                    class="nav-item {{ request()->routeIs('ambiente-virtual.index') ? 'bg-primary text-white rounded' : '' }}">
+                                    <a class="nav-link" href="{{ route('ambiente-virtual.index') }}">
+                                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="icon icon-tabler icons-tabler-outline icon-tabler-checklist">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                    d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" />
+                                                <path d="M14 19l2 2l4 -4" />
+                                                <path d="M9 8h4" />
+                                                <path d="M9 12h2" />
+                                            </svg>
+                                        </span>
+                                        {{ __('Núcleo Virtual') }}
+                                    </a>
+                                </li>
+
                             @endif
                             @if (Session::get('role') !== 'aluno')
                                 @if (Session::get('verified'))
@@ -591,7 +612,7 @@
                             @endif
                             @endif
 
-                            <li
+{{--                            <li
                                 class="nav-item  {{ request()->routeIs('ead.index') ? 'bg-primary text-white rounded' : '' }}">
                                 <a class="nav-link" href="{{ route('ead.index') }}">
                                     <span class="nav-link-icon d-md-none d-lg-inline-block">
@@ -600,6 +621,7 @@
                                     {{ __('EAD') }}
                                 </a>
                             </li>
+--}}
 
                             @if (($user->role === 'professor' && $status != 0) || ($user->role !== 'professor'))
                             <li
