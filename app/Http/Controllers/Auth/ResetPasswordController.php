499b63c69d9a4afbd60f9764838afa6f2ee5b32a
Cauã <cauaserpa092@gmail.com>
2026-05-29 20:04:00 -0300

    fix: marca email como verificado após redefinição de senha

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
@@ -26,4 +28,28 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/alunos';
+
+    /**
+     * Reset the given user's password and mark email as verified.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $this->setUserPassword($user, $password);
+
+        $user->setRememberToken(Str::random(60));
+
+        if (method_exists($user, 'hasVerifiedEmail') && ! $user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+        }
+
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        $this->guard()->login($user);
+    }
 }
